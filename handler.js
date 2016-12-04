f93f46375163ff37f724cb0b1b62a33fe8a59116
takuseno <takuma.seno@gmail.com>
2016-12-04 11:32:13 +0900

    Initial commit

---
+++ handler.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const MailParser = require("mailparser").MailParser
+
+module.exports.parse = (event, context, callback) => {
+  const raw = event.json.mime
+  const mime = new Buffer(raw, 'base64').toString()
+  const parser = new MailParser()
+
+  parser.on('end', (mail) => {
+    const response = {
+      from: mail.from,
+      to: mail.to,
+      bcc: mail.bcc,
+      cc: mail.cc,
+      subject: mail.subject,
+      html: mail.html,
+      date: mail.date.toJSON()
+    }
+    callback(null, response)
+  })
+
+  parser.write(mime)
+  parser.end()
+}
